6bd1e5e4d53bfad77fe2c39376a57fb6d484bd33
davemes1 <daveunity@gmail.com>
2025-02-21 20:36:13 -0500

    NetflixFinal

--- Netflix/src/componenets/Banner/Banner.jsx
+++ Netflix/src/componenets/Banner/Banner.jsx
@@ -22,19 +22,21 @@ export default function Banner() {
 
  },[]);
 
- const truncate= (text,length=200) =>{
+ const truncate = ( (text,length=200) =>{
     if(text?.length <= length){
         return text;
+     } else{
+      return text?.slice(0,length) + '...';
      }
-     return text.slice(0,length) + '...';
+     
 
- }; 
+ });
  
 
 
   return (
     <div
-      className="banner"
+      className="banner_container"
       style={{
         backgroundSize: "cover",
         backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
@@ -44,19 +46,19 @@ export default function Banner() {
         
       }}
     >
-      <div className="banner_contents">
-        <h1 className="banner_title">{movie.title || movie.name || movie.originalname}</h1>
-        <div className="banner_buttons">
-          <button className="banner_button play">Play</button>
-          <button className="banner_button">My List</button>
+      <div className="banner_container_content">
+        <h1>{movie.title || movie.name || movie.originalname}</h1>
+        <div className="banner_button">
+          <button >Play</button>
+          <button >My List</button>
         </div>
         <div>
-        {/* <p className="banner_description"> {truncate(movie?.overview,150)} </p> */}
-        <p>{movie?.overview}</p>
+        {/* <p {truncate(movie?.overview,150)} </p> */}
+        <p>{truncate(movie?.overview)}</p>
         </div>
         
       </div>
-      < div className = "banner_fadebottom"/>
+      < div className = "shadow"/>
     </div>
   );
 }
